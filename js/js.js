38d52789314db4a73382174785aeced2e0602c5f
Fauzan <faauzan122@gmail.com>
2026-01-31 01:37:32 +0700

    update v.1.1 * penambahan banyak fitur

--- js/js.js
+++ js/js.js
@@ -1,3 +1,11 @@
+//matikan fitur auto scroll default
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+}
+// event
 window.addEventListener("load", function () {
-  document.getElementById("loading-screen").style.display = "none";
+  document.getElementById("loading-screen").classList.add("hide-anim");
+  AOS.init({
+    once: true,
+  });
 });
