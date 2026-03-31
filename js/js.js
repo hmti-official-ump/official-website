61d31193dc7ce31e4ae57aa54ca3ef1cd13ecb49
Fauzan <faauzan122@gmail.com>
2026-04-01 03:10:09 +0700

    Update Besar v1.2 * perbaikan desain pada berbagai halaman * perbaikan hero pada struktur, album, tentang * penambahan konten pada berbagai halaman * penambahan album baru * penghapusan pembagian halaman divisi * lainnya

--- js/js.js
+++ js/js.js
@@ -9,3 +9,25 @@ window.addEventListener("load", function () {
     once: true,
   });
 });
+
+// effect active
+function setAktif() {
+  document.querySelectorAll(".divisi-nav .nav-link").forEach((el) => el.classList.remove("active"));
+  const hash = location.hash || "#bph";
+  const target = document.querySelector(`.divisi-nav a[href="${hash}"]`);
+
+  if (target) {
+    target.classList.add("active");
+  }
+}
+
+// scroll effek
+document.getElementById("cta").onclick = () => {
+  window.scrollBy({
+    top: 500, // jarak scroll (px)
+    behavior: "smooth",
+  });
+};
+
+window.addEventListener("load", setAktif());
+window.addEventListener("hashchange", setAktif);
