958f031e4626d1e91b1896e0ce94f9ead57a21f8
fauzanfazan122 <faauzan122@gmail.com>
2026-04-13 02:58:48 +0700

    Update Sedang v1.2 * Update AOS * Update struktur * Update konten beberapa halaman

--- js/js.js
+++ js/js.js
@@ -4,15 +4,25 @@ if ("scrollRestoration" in history) {
 }
 // event
 window.addEventListener("load", function () {
-  document.getElementById("loading-screen").classList.add("hide-anim");
+  document.getElementById("loading-screen")?.classList.add("hide-anim");
+
   AOS.init({
     once: true,
+    offset: 120,
+    duration: 800,
+    easing: "ease-in-out",
   });
+
+  setTimeout(() => {
+    AOS.refresh();
+  }, 100);
 });
 
 // effect active
 function setAktif() {
-  document.querySelectorAll(".divisi-nav .nav-link").forEach((el) => el.classList.remove("active"));
+  document
+    .querySelectorAll(".divisi-nav .nav-link")
+    .forEach((el) => el.classList.remove("active"));
   const hash = location.hash || "#bph";
   const target = document.querySelector(`.divisi-nav a[href="${hash}"]`);
 
@@ -29,5 +39,14 @@ document.getElementById("cta").onclick = () => {
   });
 };
 
-window.addEventListener("load", setAktif());
-window.addEventListener("hashchange", setAktif);
+window.addEventListener("hashchange", function () {
+  setAktif();
+
+  document.querySelectorAll(".card[data-aos]").forEach((el) => {
+    el.classList.remove("aos-animate");
+  });
+
+  AOS.refreshHard();
+});
+
+window.addEventListener("load", setAktif);
